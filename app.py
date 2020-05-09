dcf19a322ed2a701183164c8f2a63564f1c0f39a
Marc Cenac <marc.j.cenac@gmail.com>
2020-05-09 11:12:01 -0500

    Refactoring

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import logging
+import os
 
-from utils import write_output_file
+import pandas as pd
+
+from utils import remove_first_rows, write_output_file
 
 logging.basicConfig(
     level=logging.INFO,
@@ -25,13 +28,13 @@ from cleanup_keep_calm_with_covid import (
     CONVERTERS,
 )
 
-UPLOAD_FOLDER = "/tmp/uploads/"
-DOWNLOAD_FOLDER = "/tmp/downloads/"
-ALLOWED_EXTENSIONS = {"csv", "CSV"}
+UPLOADS_DIR = "/tmp/uploads/"
+DOWNLOADS_DIR = "/tmp/downloads/"
+ALLOWED_EXTENSIONS = {"csv"}
 
 app = Flask(__name__, static_url_path="/public", template_folder="public")
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config["DOWNLOAD_FOLDER"] = DOWNLOAD_FOLDER
+app.config["UPLOADS_DIR"] = UPLOADS_DIR
+app.config["DOWNLOADS_DIR"] = DOWNLOADS_DIR
 
 # https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/#improving-uploads
 # limit upload size upto 10MB
@@ -53,41 +56,43 @@ def index():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            output_filename = f"{filename.split('.')[0]}.xlsx"
-            process_file(
-                "", os.path.join(app.config["UPLOAD_FOLDER"], filename), output_filename
+            uploaded_filename = os.path.join(app.config["UPLOADS_DIR"], filename)
+            file.save(uploaded_filename)
+            output_filename = f"{filename.rsplit('.', 1)[0]}_cleaned.xlsx"
+            download_filename = os.path.join(
+                app.config["DOWNLOADS_DIR"], output_filename
             )
-            return redirect(url_for("uploaded_file", filename=output_filename))
+            process_file(request.form["script_name"], uploaded_filename, download_filename)
+            return send_from_directory(
+                app.config["DOWNLOADS_DIR"], output_filename, as_attachment=True
+            )
     return render_template("index.html")
 
 
-def process_file(script_name, input_path, output_filename):
+def process_file(script_name, uploaded_filename, download_filename):
     print(
-        f"processing file from {input_path} and writing to {output_filename} {os.path.join(app.config['DOWNLOAD_FOLDER'], output_filename)}"
+        f"processing file from {uploaded_filename} and writing to {download_filename}"
     )
     # this could be a good use case for the strategy pattern
-    import pandas as pd
-
-    df = pd.read_csv(input_path, encoding="ISO-8859-1", converters=CONVERTERS)
-    columns = df.iloc[1].values.tolist()
-    df = df.iloc[2:]
-    df.columns = columns
-    logging.info("Cleaning data for Keep Calm with COVID Dashboard")
-    cleanup_keep_calm_with_covid(df)
-    logging.info(
-        f"Writing data for Keep Calm with COVID Dashboard to '{output_filename}'"
-    )
-    write_output_file(df, os.path.join(app.config["DOWNLOAD_FOLDER"], output_filename))
-
-
-@app.route("/uploads/<filename>")
-def uploaded_file(filename):
-    return send_from_directory(
-        app.config["DOWNLOAD_FOLDER"], filename, as_attachment=True
-    )
+    if script_name == "keep_calm_with_covid":
+        df = pd.read_csv(
+            uploaded_filename, encoding="ISO-8859-1", converters=CONVERTERS
+        )
+        df = remove_first_rows(df)
+        logging.info("Cleaning data for Keep Calm with COVID Dashboard")
+        df = cleanup_keep_calm_with_covid(df)
+        logging.info(
+            f"Writing data for Keep Calm with COVID Dashboard to '{download_filename}'"
+        )
+        write_output_file(df, download_filename)
+    else:
+        print("not implemented")
 
 
 if __name__ == "__main__":
+    for directory in [UPLOADS_DIR, DOWNLOADS_DIR]:
+        if not os.path.exists(directory):
+            print(f"creating {directory}")
+            os.makedirs(directory)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
