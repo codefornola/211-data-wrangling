d671aaafae2bba71b175a2346707dc29c62c4d40
Marc Cenac <marc.j.cenac@gmail.com>
2020-05-10 09:59:22 -0500

    Update to handle multiple files/scripts

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ from flask import (
     flash,
     request,
     redirect,
-    url_for,
     render_template,
     send_from_directory,
 )
@@ -27,6 +26,7 @@ from cleanup_keep_calm_with_covid import (
     cleanup as cleanup_keep_calm_with_covid,
     CONVERTERS,
 )
+from cleanup_all_covid_calls import cleanup as cleanup_all_covid_calls
 
 UPLOADS_DIR = "/tmp/uploads/"
 DOWNLOADS_DIR = "/tmp/downloads/"
@@ -48,10 +48,18 @@ def allowed_file(filename):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        if "file" not in request.files:
-            flash("No file part")
+        if request.files is not None and request.form["script_name"]:
+            return process_files(request.form["script_name"], request.files)
+    return render_template("index.html")
+
+
+def process_files(script_name, files):
+    # this could be a good use case for the strategy pattern
+    if script_name == "keep_calm_with_covid":
+        if "file1" not in files:
+            print("did not have required files")
             return redirect(request.url)
-        file = request.files["file"]
+        file = request.files["file1"]
         if file.filename == "":
             return redirect(request.url)
         if file and allowed_file(file.filename):
@@ -62,31 +70,46 @@ def index():
             download_filename = os.path.join(
                 app.config["DOWNLOADS_DIR"], output_filename
             )
-            process_file(request.form["script_name"], uploaded_filename, download_filename)
+            df = pd.read_csv(
+                uploaded_filename, encoding="ISO-8859-1", converters=CONVERTERS
+            )
+            df = remove_first_rows(df)
+            logging.info("Cleaning data for Keep Calm with COVID Dashboard")
+            df = cleanup_keep_calm_with_covid(df)
+            logging.info(
+                f"Writing data for Keep Calm with COVID Dashboard to '{download_filename}'"
+            )
+            write_output_file(df, download_filename)
             return send_from_directory(
                 app.config["DOWNLOADS_DIR"], output_filename, as_attachment=True
             )
-    return render_template("index.html")
-
-
-def process_file(script_name, uploaded_filename, download_filename):
-    print(
-        f"processing file from {uploaded_filename} and writing to {download_filename}"
-    )
-    # this could be a good use case for the strategy pattern
-    if script_name == "keep_calm_with_covid":
-        df = pd.read_csv(
-            uploaded_filename, encoding="ISO-8859-1", converters=CONVERTERS
-        )
-        df = remove_first_rows(df)
-        logging.info("Cleaning data for Keep Calm with COVID Dashboard")
-        df = cleanup_keep_calm_with_covid(df)
+    if script_name == "all_covid":
+        if "file1" not in files or "file2" not in files:
+            print("did not have required files")
+            return redirect(request.url)
+        file1 = request.files["file1"]
+        file2 = request.files["file2"]
+        if file1.filename == "" or file2.filename == "":
+            print("invalid filename")
+            return redirect(request.url)
+        dfs = {}
+        dfvl = pd.read_csv(file1, encoding="ISO-8859-1")
+        dfvl = remove_first_rows(dfvl)
+        dfs["VIALINK"] = dfvl
+        df232 = pd.read_csv(file2, encoding="ISO-8859-1")
+        df232 = remove_first_rows(df232)
+        dfs["TWO32"] = df232
+        logging.info("Cleaning data for All COVID Calls Dashboard")
+        df = cleanup_all_covid_calls(dfs)
+        output_filename = f"all_covid_cleaned.xlsx"
+        download_filename = os.path.join(app.config["DOWNLOADS_DIR"], output_filename)
         logging.info(
-            f"Writing data for Keep Calm with COVID Dashboard to '{download_filename}'"
+            f"Writing data for All COVID Calls Dashboard to '{download_filename}'"
         )
         write_output_file(df, download_filename)
-    else:
-        print("not implemented")
+        return send_from_directory(
+            app.config["DOWNLOADS_DIR"], output_filename, as_attachment=True
+        )
 
 
 if __name__ == "__main__":
