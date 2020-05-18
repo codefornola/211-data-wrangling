d2736e62c5b33ea719ccea7d122057778fa89181
Ryan B. Harvey <ryan.b.harvey@ted.com>
2020-05-18 02:06:51 -0500

    Implement fixtures, mocks, stubs and test coverage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,35 @@
-from cleanup.utils import get_lat, get_lng
+import os
+import pandas as pd
+from io import BytesIO
+
+from fixtures import df
+
+from cleanup.utils import write_output_file, get_lat, get_lng
+
+
+def test_write_output_file(df, mocker):
+  # mocks & stubs
+  mocker.patch.object(pd.DataFrame, 'to_excel')
+  mocker.patch('os.makedirs')
+  path_exists_stub = mocker.stub('os.path.exists')
+  # it calls to_excel on the file
+  filename = 'nondata/somefile.xlsx'
+  write_output_file(df, filename)
+  pd.DataFrame.to_excel.assert_called_once_with(filename)
+  # it doesn't call os.makedirs when not prefixed with 'data'
+  filename = 'nondata/somefile.xlsx'
+  write_output_file(df, filename)
+  os.makedirs.assert_not_called()
+  # it doesn't call os.makedirs when prefixed with 'data' & exists
+  filename = 'data/somefile.xlsx'
+  path_exists_stub.return_value = True
+  write_output_file(df, filename)
+  os.makedirs.assert_not_called()
+  # it calls os.makedirs when prefixed with 'data', but doesn't exist
+  filename = 'data/somefile.xlsx'
+  path_exists_stub.return_value = False
+  write_output_file(df, filename)
+  os.makedirs.assert_not_called()
 
 
 def test_get_lat():
